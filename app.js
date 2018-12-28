e55438dbb8dc20f35a5ba7048a6e37bc21f952e4
kiranbhalerao123 <bhaleraok65@gmail.com>
2018-12-28 14:48:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+// BODY-PARSER MIDDLEWARE
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+)
+
+// HANDLEBARS MIDDLEWARE
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('index', { stripePublicKey: process.env.STRIPE_PUBLIC_KEY })
+})
+
+app.get('/success', (req, res) => {
+  res.render('success')
+})
+
+app.post('/charge', (req, res) => {
+  const amount = 2000
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken
+    })
+    .then(customer => {
+      return stripe.charges.create({
+        amount,
+        currency: 'usd',
+        customer: customer.id,
+        description: 'Happy Potter Ebook'
+      })
+    })
+    .then(charge => {
+      // New charge created on a new customer
+      return res.render('success')
+    })
+    .catch(err => {
+      // Deal with an error
+      return res.send(err.message)
+    })
+})
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`))
